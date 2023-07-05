a06e05dafae467800e609782153d9f996c32870b
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2023-07-05 18:18:08 +0200

    Add project files.

---
+++ Binary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace Binary
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Random random = new Random();
+
+            while (true)
+            {
+                Console.Write(random.Next(2));
+            }
+        }
+    }
+}
